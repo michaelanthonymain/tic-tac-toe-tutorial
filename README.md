7c27df06081d2164a0a512e923067ae8beb34f6d
Michael Main <michaelanthonymain@gmail.com>
2016-11-05 14:12:22 -0500

    WIP commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### TODO
+1. Add babel
+2. Add directory structure
+3. Get hello world working
+4. Move on to tic tac toe tutorial
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+)
+
 require('./style.css');
 document.write(require('./content.js'));
