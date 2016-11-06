66378b7016c6e0ce9b484b536e74e9f113b7b479
Michael Main <michaelanthonymain@gmail.com>
2016-11-06 13:12:14 -0600

    functional component

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-    firstName: 'Michael',
-    lastName: 'Main'
-};
-
 const element = (
-    <h1>
-        Hello, { formatName(user) }!
-    </h1>
+    <Welcome name="Sara" />
 );
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
 
 ReactDOM.render(
     element,
     document.getElementById('root')
 );
-
-function tick() {
-    const element = (
-        <div>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('clock')
-    );
-}
-
-setInterval(tick, 1000);
