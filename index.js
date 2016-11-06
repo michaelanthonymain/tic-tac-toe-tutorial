0d3b25317ecc6f605658a4d5e82b80334b1d67d6
Michael Main <michaelanthonymain@gmail.com>
2016-11-06 13:01:09 -0600

    render element with jsx

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-require('./style.css');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Michael',
+    lastName: 'Main'
+};
+
+const element = (
+    <h1>
+        Hello, { formatName(user) }!
+    </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
